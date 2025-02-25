7c00631158f93aca209cf89b9d65e8fa26e3f56d
devCJH <http-@naver.com>
2025-02-25 11:12:41 +0900

    article modify, 중복로직 method분리, Article 클래스 분리

--- src/com/koreaIT/BAM/Main.java
+++ src/com/koreaIT/BAM/Main.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Main {
+import com.koreaIT.BAM.dto.Article;
+
+public class Main {
+	
+	private static List<Article> articles;
+	
+	static {
+		articles = new ArrayList<>();
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("== 프로그램 시작 ==");
@@ -12,8 +21,6 @@ class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int lastArticleId = 0;
-		
-		List<Article> articles = new ArrayList<>();
 		
 		while (true) {
 			System.out.printf("명령어) ");
@@ -24,6 +31,7 @@ class Main {
 			}
 			
 			if (cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
@@ -42,31 +50,57 @@ class Main {
 					continue;
 				}
 				
+				System.out.println("== 게시물 목록 ==");
 				System.out.println("번호	|	제목");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d	|	%s\n", article.id, article.title);
+					System.out.printf("%d	|	%s\n", article.getId(), article.getTitle());
 				}
 				
 			} else if (cmd.startsWith("article detail ")) {
-				String[] cmdBits = cmd.split(" ");
-				int id = Integer.parseInt(cmdBits[2]);
+				int id = getCmdNum(cmd);
 				
-				boolean a = false; 
-				
-				for (Article article : articles) {
-					if (article.id == id) {
-						System.out.printf("번호 : %d\n", article.id);
-						System.out.printf("제목 : %s\n", article.title);
-						System.out.printf("내용 : %s\n", article.body);
-						a = true;
-						break;
-					}
+				if (id == -1) {
+					System.out.println("잘못된 명령어입니다");
+					continue;
 				}
 				
-				if (a == false) {
+				Article foundArticle = getArticleById(id);
+				
+				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
 				}
+				
+				System.out.println("== 게시물 상세보기 ==");
+				System.out.printf("번호 : %d\n", foundArticle.getId());
+				System.out.printf("제목 : %s\n", foundArticle.getTitle());
+				System.out.printf("내용 : %s\n", foundArticle.getBody());
+				
+			} else if (cmd.startsWith("article modify ")) {
+				int id = getCmdNum(cmd);
+				
+				if (id == -1) {
+					System.out.println("잘못된 명령어입니다");
+					continue;
+				}
+				
+				Article foundArticle = getArticleById(id);
+				
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				
+				System.out.println("== 게시물 수정 ==");
+				System.out.printf("수정할 제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("수정할 내용 : ");
+				String body = sc.nextLine();
+
+				foundArticle.setTitle(title);
+				foundArticle.setBody(body);
+				System.out.printf("%d번 게시물을 수정했습니다\n", id);
 			}
 		}
 		
@@ -74,16 +108,23 @@ class Main {
 		
 		System.out.println("== 프로그램 종료 ==");
 	}
-}
 
-class Article {
-	int id;
-	String title;
-	String body;
-	
-	Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
+	private static int getCmdNum(String cmd) {
+		try {
+			String[] cmdBits = cmd.split(" ");
+			int id = Integer.parseInt(cmdBits[2]);
+			return id;
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+	}
+
+	private static Article getArticleById(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
 	}
 }
